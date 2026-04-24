266340a9db94aee2ea0901f94fc063953794651e
3la2a7mad <alaa056945@gmail.com>
2026-04-24 16:56:07 +0300

    add isPalindrome file

--- main.js
+++ main.js
@@ -1,17 +1,1 @@
-function isPalindrome(arr) {
-    let i = 0;
-    let j = arr.length - 1;
-
-    while (i < j) {
-        if (arr[i] !== arr[j]) {
-            return false; 
-        }
-        i++;
-        j--;
-    }
-
-    return true; 
-}
-
-var arr = [1, 2, 1];
-console.log(isPalindrome(arr)); 
+console.log("Hello, World!");
